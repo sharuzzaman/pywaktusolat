64698aa8b702f83c2c1828661e086fe3e11f561d
Sharuzzaman Ahmat Raslan <sharuzzaman@gmail.com>
2022-11-29 22:08:09 +0800

    updated solat.py
    updated code to be compatible with Python 3.x . change urllib2 to urllib.request. change e-solat URL to the latest known working version

--- solat.py
+++ solat.py
@@ -1,24 +1,25 @@
 #!/bin/env python
 
-import urllib2
+import urllib.request
 import xml.etree.ElementTree as ET
 
-response = urllib2.urlopen('http://www2.e-solat.gov.my/xml/today/?zon=SGR04')
+response = urllib.request.urlopen(
+  'https://www.e-solat.gov.my/index.php?r=esolatApi/xmlfeed&zon=SGR01')
 html = response.read()
 
 root = ET.fromstring(html)
 
 for channel in root.iter('channel'):
-    kawasan = channel.find('link').text
+  kawasan = channel.find('link').text
 
-jadual=[]
+jadual = []
 
 for item in root.iter('item'):
-    title=item[0].text
-    desc=item[1].text
-    waktu=[title,desc]
-    jadual.append(waktu)
+  title = item[0].text
+  desc = item[1].text
+  waktu = [title, desc]
+  jadual.append(waktu)
 
-print "Waktu solat bagi kawasan %s" % kawasan
+print("Waktu solat bagi kawasan %s" % kawasan)
 for solat, masa in jadual:
-    print solat, masa
+  print(solat, masa)
